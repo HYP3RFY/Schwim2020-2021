0e2d3a0eca4b36ac53d2406b60a8db84c3bf1508
Will Montello <willmontello@gmail.com>
2020-12-09 16:33:13 -0500

    Code as of 12/9/2020
    Finished code for robot, no auton or vision.

--- src/main.cpp
+++ src/main.cpp
@@ -5,12 +5,13 @@
 using namespace pros;
 Controller master(E_CONTROLLER_MASTER);
 Motor leftMtr(1);
-Motor rightMtr(2);
+Motor rightMtr(10);
 Motor elevatorMtr(3);
 Motor switchMtr(4);
 Motor leftIntake(5);
 Motor rightIntake(6);
 Motor bottomIntake(7);
+Motor extakeMtr(20);
 
 
 bool elevatorToggle = false;
@@ -25,6 +26,7 @@ int eleVar = 0;
  */
 void initialize() {
 	lcd::initialize();
+	switchMtr.set_brake_mode(MOTOR_BRAKE_HOLD);
 }
 
 /**
@@ -32,7 +34,9 @@ void initialize() {
  * the VEX Competition Switch, following either autonomous or opcontrol. When
  * the robot is enabled, this task will exit.
  */
-void disabled() {}
+void disabled() {
+	switchMtr.set_brake_mode(MOTOR_BRAKE_HOLD);
+}
 
 /**
  * Runs after initialize(), and before autonomous when connected to the Field
@@ -43,7 +47,9 @@ void disabled() {}
  * This task will exit when the robot is enabled and autonomous or opcontrol
  * starts.
  */
-void competition_initialize() {}
+void competition_initialize() {
+	switchMtr.set_brake_mode(MOTOR_BRAKE_HOLD);
+}
 
 /**
  * Runs the user autonomous code. This function will be started in its own task
@@ -56,7 +62,9 @@ void competition_initialize() {}
  * will be stopped. Re-enabling the robot will restart the task, not re-start it
  * from where it left off.
  */
-void autonomous() {}
+void autonomous() {
+	switchMtr.set_brake_mode(MOTOR_BRAKE_HOLD);
+}
 
 /**
  * Runs the operator control code. This function will be started in its own task
@@ -74,18 +82,20 @@ void autonomous() {}
 void opcontrol() {
 	Controller master(E_CONTROLLER_MASTER);
 	Motor leftMtr(1);
-	Motor rightMtr(2);
+	Motor rightMtr(10);
 	Motor elevatorMtr(3);
 	Motor switchMtr(4);
 	Motor leftIntake(5);
 	Motor rightIntake(6);
 	Motor bottomIntake(7);
+	Motor extakeMtr(20);
 
 	leftMtr.set_brake_mode(MOTOR_BRAKE_BRAKE);
 	rightMtr.set_brake_mode(MOTOR_BRAKE_BRAKE);
 	elevatorMtr.set_brake_mode(MOTOR_BRAKE_BRAKE);
-	switchMtr.set_brake_mode(MOTOR_BRAKE_BRAKE);
+	switchMtr.set_brake_mode(MOTOR_BRAKE_HOLD);
 	elevatorMtr.set_gearing(MOTOR_GEARSET_6);
+	extakeMtr.set_gearing(MOTOR_GEARSET_6);
 	switchMtr.set_gearing(MOTOR_GEARSET_6);
 
 	while (true) {
@@ -97,22 +107,25 @@ Drive Train
 		int left = power + turn;
 	 	int right = power - turn;
 		leftMtr.move_velocity(left);
-	 	rightMtr.move_velocity(right);
+	 	rightMtr.move_velocity(-right);
 /*--------------------------------------------
 Elevator Control
 ----------------------------------------------*/
-		if (master.get_digital(DIGITAL_R1)) {
+		if (master.get_digital(DIGITAL_R2)) {
 			leftIntake.move_velocity(200);
 			rightIntake.move_velocity(-200);
-			bottomIntake.move_velocity(200);
-		} else if (master.get_digital(DIGITAL_R2)) {
+			bottomIntake.move_velocity(-200);
+			elevatorMtr.move_velocity(600);
+		} else if (master.get_digital(DIGITAL_R1)) {
 			leftIntake.move_velocity(-200);
 			rightIntake.move_velocity(200);
-			bottomIntake.move_velocity(-200);
+			bottomIntake.move_velocity(200);
+			elevatorMtr.move_velocity(-600);
 		} else {
 			leftIntake.move_velocity(0);
 			rightIntake.move_velocity(0);
 			bottomIntake.move_velocity(0);
+			elevatorMtr.move_velocity(0);
 		}
 /*--------------------------------------------
 Elevator Toggle
@@ -120,29 +133,24 @@ Elevator Toggle
 		if (master.get_digital_new_press(DIGITAL_X)) {
 			eleVar = 1 - eleVar;
 		}
-/*--------------------------------------------
-Switch Control
-----------------------------------------------*/
-		if (master.get_digital(DIGITAL_L2)){
-		  elevatorToggle = true;
-		} else {
-			elevatorToggle = false;
-		}
+
 		/*--------------------------------------------
 Elevator Control
 ----------------------------------------------*/
-		if (elevatorToggle == true && eleVar == 1) {
-			switchMtr.move_velocity(600);
-			elevatorMtr.move_velocity(600);
-		} else if (elevatorToggle == false && eleVar == 1){
+		if (master.get_digital(DIGITAL_L2)) {
 			switchMtr.move_velocity(-600);
-			elevatorMtr.move_velocity(600);
+			elevatorMtr.move_velocity(-600);
+		} else if (master.get_digital(DIGITAL_L1)){
+			switchMtr.move_velocity(600);
+			elevatorMtr.move_velocity(-600);
+			extakeMtr.move_velocity(600);
 		} else {
 			switchMtr.move_velocity(0);
-			elevatorMtr.move_velocity(0);
+			extakeMtr.move_velocity(0);
 		}
-
-
+		if (master.get_digital(DIGITAL_X)) {
+			extakeMtr.move_velocity(-600);
+		}
 /*	if (master.get_digital(DIGITAL_L1)) {
 		elevatorMtr.move_velocity(600);
 		switchMtr.move_velocity(-600);
